2b673e54b9eabaa139641356cbef7b363e4735eb
Jacob Allen <47609209+bocajbee@users.noreply.github.com>
2020-09-22 19:35:29 -0700

    Update HighlightMyParks.js

--- Final Project/static/js/HighlightMyParks.js
+++ Final Project/static/js/HighlightMyParks.js
@@ -1,11 +1,17 @@
+// global variable to "hold" the div ID of the currently selected/highlioghted park
+// take all of the divs by the element "list_item_myparks" and store them in an array to loop through later
 var currently_highlighted_park;
-let highlighted_park_div = document.getElementsByClassName("list_item");
+console.log(currently_highlighted_park);
+let init_highlighted_park_div = document.getElementsByClassName("list_item_myparks");
 
-for (let i = 0; i < highlighted_park_div.length; i++) {
-  highlighted_park_div[i].onclick = function(){
-
+// loop through this "init_highlighted_park_div array" containing all of the divs for this class
+for (let i = 0; i < init_highlighted_park_div.length; i++) {
+  // subscribe this function to the div in the index being looped through when one of the divs in this array is clicked
+  init_highlighted_park_div[i].onclick = function(){
+    console.log(currently_highlighted_park);
     if (currently_highlighted_park) document.getElementById(currently_highlighted_park).style.backgroundColor = "white";
-    currently_highlighted_park = highlighted_park_div[i].id;
-    highlighted_park_div[i].style.backgroundColor = "#f2f2f2";
+    currently_highlighted_park = init_highlighted_park_div[i].id;
+    console.log(currently_highlighted_park);
+    init_highlighted_park_div[i].style.backgroundColor = "#f2f2f2";
   }
 }
