f624ea2f9c66d0ee3bf4455d32321b460dc01327
Jacob Allen <47609209+bocajbee@users.noreply.github.com>
2020-10-21 15:44:52 -0700

    Update reviews.js

--- Final Project/static/js/reviews.js
+++ Final Project/static/js/reviews.js
@@ -2,6 +2,7 @@
 // global variable to "hold" the div ID of the currently selected/highlioghted park
 // https://www.youtube.com/watch?v=oUpEKosnC8E&list=PL4cUxeGkcC9gfoKa5la9dsdCNpuey2s-V&index=3
 // store the colected elements into a HTML collection
+
 var currently_highlighted_review;
 var init_highlighted_review_div = document.getElementsByClassName("review_parks");
 
@@ -18,9 +19,9 @@ for (let i = 0; i < init_highlighted_review_div.length; i++) {
 
 // use JS to grab every button on my page by their designated class_name "go_to_on_map"
 // these buttons by their clase name are stored in an array, into a single variable
+// loop through every button in this array by its collected "element", add a listener to each index to detect when the [i]th "go_to_on_map" button looped through is clicked & store it inside "button_id"
 let goto_map_button = document.getElementsByClassName("go_to_on_map");
 
-// loop through every button in this array by its collected "element", add a listener to each index to detect when the [i]th "go_to_on_map" button looped through is clicked & store it inside "button_id"
 for (let i = 0; i < goto_map_button.length; i++){
     goto_map_button[i].onclick = function(){
         button_id = goto_map_button[i].id;
@@ -33,12 +34,13 @@ for (let i = 0; i < goto_map_button.length; i++){
 // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onload
 // https://stackoverflow.com/questions/12627443/jquery-click-vs-onclick
 // https://stackoverflow.com/questions/588040/window-onload-vs-document-onload
+
 window.onload = function() {
   var clicked_button_id = localStorage.getItem('button_id');
   console.log(clicked_button_id);
 
+  // https://stackoverflow.com/questions/17569012/simulate-a-click-on-a-element-using-javascript-jquery\
   if (clicked_button_id){
-    // https://stackoverflow.com/questions/17569012/simulate-a-click-on-a-element-using-javascript-jquery\
     var clicked_park_id = document.getElementById("Park " + clicked_button_id);
     console.log(clicked_park_id);
     clicked_park_id.click();
@@ -50,6 +52,7 @@ window.onload = function() {
 // JS script to filter the divs by inout in the text search box
 // grab the id/reference to the form we want & attach an event listener to this search bar's/texts keyup() presseses (not working)
 // https://stackoverflow.com/questions/12402101/store-data-from-a-form-into-a-javascript-variable/12402127
+
 function TextSearch() {
   const searchText = document.getElementById('form-text').value
   const SearchTerm = searchText.toLowerCase();
@@ -59,6 +62,7 @@ function TextSearch() {
   // loop through the HTML collection of divs
   // grab the skatepark name from the "title" tag of the div being looped through in the HTML collection
   // https://www.w3schools.com/jsref/jsref_indexof.asp
+
   for (let i = 0; i < review_parks_tohide.length; i++){
     const title = review_parks_tohide[i].title;
     if (!SearchTerm || title.toLowerCase().indexOf(SearchTerm) != -1) {
